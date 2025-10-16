299b2132940746fd78aa7ddc2e4b732558da8d86
FrancescoTroise <francesco.troise98@gmail.com>
2025-10-16 16:32:12 +0200

    Add "check-results" node selection
    Add a block to the "create_input" function to generate the "check-results" button.

--- asstes/javascript/script.js
+++ asstes/javascript/script.js
@@ -1,9 +1,12 @@
 const btn_start = document.getElementById("btn-start")
+//bottone start per dare inizio al gioco
 const box_number = document.getElementById("box-number")
 //<div> dove verranno inseriti i numeri generati
 const box_input = document.getElementById("box-input")
-//<div> dove verranno inseriti gli <input> geenrati lato javascript
-//Seleziono gli elementi della DOM
+//<div> dove verranno inseriti gli <input> generati lato javascript
+const check_result = document.getElementById("check-results")
+//<div> contenete paragrafo nascosto e che accoglierà il bottone di verifica
+//Selezione degli elementi della DOM
 
 function random_number(){
 //Funzione per la generazione di numeri casuali(da 1 a 99)
@@ -24,16 +27,33 @@ function clear_number(){
 function create_input(){
     for(let i = 0; i < 5; i++){
         const input = document.createElement("input")
+        //Genera -input(come effettivo nodo della DOM) e lo salva, lato javascript, nella variabile "input"
         input.id = "user-num" + i
+        //Associa -id diversi ai diversi -input
         input.type = "number"
+        //Definisce il -type del -input
         input.placeholder = "Inserire qui il numero"
+        //Definisce -placeholder del -input
 
         box_input.appendChild(input)
+        //Aggiunge il nodo generato (-input) all'interno del <div> dedicato
     }
+
+    const btn_results = document.createElement("button")
+    //Genera -button(come effettivo nodo della DOM) e lo salva, lato javascript, nella variabile "btn_results"
+    btn_results.id = "btn-check"
+    //Associa un -id al -button
+    btn_results.type = "button"
+    //Associa un -type al -button
+    btn_results.classList.add("btn", "btn-light", "mt-3")
+    //Definisce classi con le quali il -button deve generarsi
+    btn_results.innerText = "Clicca per verifica"
+    //Aggiunge testo all'interno del -button
+
+    check_result.appendChild(btn_results)
+    //Aggiunge il nodo generato(-button) all'interno del <div> dedicato
 }
 
-
-    
 
 
 
